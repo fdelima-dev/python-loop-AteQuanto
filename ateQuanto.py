dfa316855c65a45c4e9fddb850257e60396426ea
fdelima-dev <fdelima.dev@gmail.com>
2026-03-04 17:05:40 -0300

    De zero até quanto?

---
+++ ateQuanto.py
@@ -0,0 +1,7 @@
+contador = 0;
+quanto = int(input('Digite até quanto vai contar\n'))
+salto = int(input('Digite o valor do salto\n'))
+
+while contador <= quanto:
+    print(contador)
+    contador = contador + salto
